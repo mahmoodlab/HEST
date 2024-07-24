53eae0824da93af6ce29952a5e73f1e67bbee339
Paul Doucet <homedoucetpaul@gmail.com>
2024-07-24 16:46:24 -0400

    refactoring of hest data shape mechanism

--- src/hest/LazyShapes.py
+++ src/hest/LazyShapes.py
@@ -1,4 +1,10 @@
+import geopandas as gpd
+import pandas as pd
+from shapely import Polygon
+
+from hest.io.seg_readers import read_gdf
 from hest.utils import verify_paths
+
 
 class LazyShapes:
     
@@ -7,8 +13,42 @@ class LazyShapes:
         self.path = path
         self.name = name
         self.coordinate_system = coordinate_system
-        self.shapes = None
+        self._shapes = None
         
-    def compute(self):
-        if self.shapes is None:
-            self.shapes = None
+    def compute(self) -> None:
+        if self._shapes is None:
+            self.shapes = read_gdf(self.path)
+            
+    @property
+    def shapes(self) -> gpd.GeoDataFrame:
+        if self._shapes is None:
+            self.compute()
+
+        return self._shapes
+    
+    def __repr__(self) -> str:
+        sup_rep = super().__repr__()
+        
+        loaded_rep = 'loaded' if self._shapes is not None else 'not loaded'
+        
+        rep = f"""name: {self.name}, coord-system: {self.coordinate_system}, <{loaded_rep}>"""
+        return rep
+    
+
+def convert_old_to_gpd(contours_holes, contours_tissue) -> gpd.GeoDataFrame:
+    assert len(contours_holes) == len(contours_tissue)
+    
+    shapes = []
+    tissue_ids = []
+    for i in range(len(contours_holes)):
+        tissue = contours_tissue[i]
+        shapes.append(Polygon(tissue[:, 0, :]))
+        tissue_ids.append(i)
+        holes = contours_holes[i]
+        if len(holes) > 0:
+            for hole in holes:
+                shapes.append(Polygon(hole[:, 0, :]))
+                tissue_ids.append(i)
+            
+    return gpd.GeoDataFrame(pd.DataFrame(tissue_ids, columns=['tissue_id']), geometry=shapes)
+        
--- src/hest/utils.py
+++ src/hest/utils.py
@@ -1,10 +1,12 @@
 import concurrent.futures
+import functools
 import gzip
 import json
 import os
 import shutil
 from enum import Enum
 from typing import List, Tuple, Union
+import warnings
 
 import h5py
 import matplotlib.pyplot as plt
@@ -29,6 +31,21 @@ from tqdm import tqdm
 
 Image.MAX_IMAGE_PIXELS = 93312000000
 ALIGNED_HE_FILENAME = 'aligned_fullres_HE.tif'
+
+
+def deprecated(func):
+    """This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emitted
+    when the function is used."""
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        warnings.simplefilter('always', DeprecationWarning)  # turn off filter
+        warnings.warn("Call to deprecated function {}.".format(func.__name__),
+                      category=DeprecationWarning,
+                      stacklevel=2)
+        warnings.simplefilter('default', DeprecationWarning)  # reset filter
+        return func(*args, **kwargs)
+    return new_func
 
 
 def verify_paths(paths):
