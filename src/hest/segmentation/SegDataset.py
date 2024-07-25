5efa76f274ca614c1604ae37354c18a6802cd148
Paul Doucet <homedoucetpaul@gmail.com>
2024-07-25 15:45:52 -0400

    speed up tissue segmentation

--- src/hest/segmentation/SegDataset.py
+++ src/hest/segmentation/SegDataset.py
@@ -1,13 +1,16 @@
 import os
+from math import floor
 
 import numpy as np
-from tqdm import tqdm
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
+from tqdm import tqdm
+
+from hest.wsi import WSI, WSIPatcher
 
 
-class SegDataset(Dataset):
+class SegFileDataset(Dataset):
     masks = []
     patches = []
     coords = []
@@ -43,3 +46,34 @@ class SegDataset(Dataset):
             sample = self.transform(sample)
 
         return sample, coord
+    
+    
+class SegWSIDataset(Dataset):
+    masks = []
+    patches = []
+    coords = []
+    
+    def __init__(self, patcher: WSIPatcher, transform):
+        self.patcher = patcher
+        
+        self.cols, self.rows = self.patcher.get_cols_rows()
+        self.size = self.cols * self.rows
+        
+        self.transform = transform
+        
+    def _index_to_col_row(self, index):
+        col = index % self.cols
+        row = index // self.rows
+        return col, row                        
+
+    def __len__(self):
+        return self.size
+    
+    def __getitem__(self, index):
+        col, row = self._index_to_col_row(index)
+        tile, x, y = self.patcher.get_tile(col, row)
+        
+        if self.transform:
+            tile = self.transform(tile)
+
+        return tile, (x, y)
