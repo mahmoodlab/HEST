59f26731f0e395ae2335b362d9396029d53b05b9
Guillaume Jaume <gjaume@bwh.harvard.edu>
2024-08-13 09:07:27 -0400

    rm unused imports (#32)
    Co-authored-by: Guillaume Jaume <guillaume.jaume@epfl.ch>

--- src/hest/bench/cpath_model_zoo/models/__init__.py
+++ src/hest/bench/cpath_model_zoo/models/__init__.py
@@ -1,8 +1,3 @@
-from .vision_transformer_latest import *
-from .vision_transformer_dinov2 import (vit_small as vit_small_dinov2, 
-                                        vit_base as vit_base_dinov2, 
-                                        vit_large as vit_large_dinov2,
-                                        clean_state_dict as clean_state_dict_dinov2)
-from .vision_transformer_ijepa import (vit_huge as vit_huge_ijepa, clean_state_dict as clean_state_dict_ijepa)                                                                 
+
 from .timm_wrappers import *
 from .hf_wrappers import *
